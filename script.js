958e4863c0125f715f7777ecd65ac18e4437ee80
stef427 <stef_427@hotmail.com>
2020-10-04 13:37:09 +0200

    final commit

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 document.getElementById("apropos").style.color = "white";
 document.getElementById("moi").style.listStyle = "none";
+
+function topFunction() {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+}
